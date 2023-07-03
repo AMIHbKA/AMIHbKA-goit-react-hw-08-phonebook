8c2a6aefeccaf214b9dce4b2aeedf91d5d455d93
AMIHbKA <95082331+AMIHbKA@users.noreply.github.com>
2023-07-03 09:58:15 +0300

    some minor fix

--- src/components/ExamplePhoneBook/ExampleContactsList/ExampleContactsList.js
+++ src/components/ExamplePhoneBook/ExampleContactsList/ExampleContactsList.js
@@ -3,9 +3,9 @@ import { ExampleListItem } from './ExampleListItem';
 import { selectFilteredExampleContactsIds } from 'redux/example/slice';
 import { Wrapper } from 'components/UI/Wrapper/Wrapper';
 import { ContactListStyled } from 'components/PhoneBook/ContactsList/ContactsList.styled';
-import { Title } from 'components/UI/Title.styles';
+// import { Title } from 'components/UI/Title.styles';
 import { InfoMessage } from 'components/UI/Messages';
-import { useTablet } from 'hooks/useWindowSize';
+// import { useTablet } from 'hooks/useWindowSize';
 
 export const ExampleContactsList = () => {
   const contactIds = useSelector(selectFilteredExampleContactsIds);
@@ -21,11 +21,11 @@ export const ExampleContactsList = () => {
       : !renderedListItems.length && filter.length
       ? 'No names or numbers were found ¯\\_ (ツ)_/¯'
       : null;
-  const isTablet = useTablet();
+  // const isTablet = useTablet();
 
   return (
     <Wrapper>
-      {isTablet && <Title>Phonebook</Title>}
+      {/* {!isTablet && <Title>Phonebook</Title>} */}
 
       {renderedListItems.length ? (
         <ContactListStyled>{renderedListItems}</ContactListStyled>
